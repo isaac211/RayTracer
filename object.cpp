b9c5efc54eb49c113378ceb4f1fe4dc161527f88
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-13 11:15:49 -0400

    Defined basic return functios.

--- object.cpp
+++ object.cpp
@@ -1,1 +1,17 @@
 #include "object.hpp"
+
+Object::Object(coords3D cent, colorType cl, lambertType lamb) : center(cent),color(cl),lambert(lamb)
+{
+}
+coords3D Object::getCenter()
+{
+	return center;
+}
+colorType Object::getColor()
+{
+	return color;
+}
+lambertType Object::getLambert()
+{
+	return lambert;
+}
--- object.hpp
+++ object.hpp
@@ -5,7 +5,10 @@ typedef float lambertType;
 class Object
 {
 public:
-	Object(coords3D center, colorType color, lambertType lambert = 0);
+	Object(coords3D cent, colorType cl, lambertType lamb = 0);
+	coords3D getCenter();
+	colorType getColor();
+	lambertType getLambert();
 
 protected:
 	coords3D center;
