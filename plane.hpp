751834366fe546531d6e91e693cec6030a005050
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-13 12:16:01 -0400

    Added inheritence from object

--- plane.hpp
+++ plane.hpp
@@ -1,3 +1,11 @@
 #ifndef _PLANE_HPP_
 #define _PLANE_HPP_
+#include "object.hpp"
+class Plane : public Object
+{
+public:
+	Plane();
+private:
+	coords3D normal;
+};
 #endif
--- sphere.hpp
+++ sphere.hpp
@@ -1,3 +1,15 @@
 #ifndef _SPHERE_HPP_
 #define _SPHERE_HPP_
+#include "object.hpp"
+
+typedef double radiusType;
+
+class Sphere : public Object
+{
+public:
+	Sphere();
+
+private:
+	radiusType radius;
+};
 #endif
