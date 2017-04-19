3e2546d4de53c644c4503bd98052edd900a2aa67
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-19 18:42:03 -0400

    Scene 1 JSOn parsing.

--- unittests.cpp
+++ unittests.cpp
@@ -43,6 +43,48 @@ TEST_CASE("Test JSON Parser", "[JSON]")
 			REQUIRE(A.getObjects()[0].getColor().b == 0);
 			REQUIRE(A.getObjects()[0].getColor().g == 0);
 			REQUIRE(A.getObjects()[0].getColor().r == 255);
+			REQUIRE(A.getObjects()[0].getLambert() == 1);
+		}
+	}
+
+	SECTION("scene 1")
+	{
+		Environment A;
+		REQUIRE_NOTHROW(A.unpackJSON("../tests/scene1.json"));
+
+		SECTION("camera")
+		{
+			REQUIRE(A.getCamera().getCenter().x == 0);
+			REQUIRE(A.getCamera().getCenter().y == 0);
+			REQUIRE(A.getCamera().getCenter().z == 0);
+			REQUIRE(A.getCamera().getFocus() == 10);
+			REQUIRE(A.getCamera().getNormal().x == 0);
+			REQUIRE(A.getCamera().getNormal().y == 0);
+			REQUIRE(A.getCamera().getNormal().z == 1);
+			REQUIRE(A.getCamera().getResolution().first == 0.01);
+			REQUIRE(A.getCamera().getResolution().second == 0.01);
+			REQUIRE(A.getCamera().getSize().first == 1024);
+			REQUIRE(A.getCamera().getSize().second == 1024);
+		}
+
+
+		SECTION("lights")
+		{
+			REQUIRE(A.getLights()[0].getIntensity() == 1);
+			REQUIRE(A.getLights()[0].getLocation().x == 5);
+			REQUIRE(A.getLights()[0].getLocation().y == -5);
+			REQUIRE(A.getLights()[0].getLocation().z == 0);
+		}
+
+		SECTION("objects")
+		{
+			REQUIRE(A.getObjects()[0].getCenter().x == 0);
+			REQUIRE(A.getObjects()[0].getCenter().y == 0);
+			REQUIRE(A.getObjects()[0].getCenter().z == 5);
+			REQUIRE(A.getObjects()[0].getColor().b == 0);
+			REQUIRE(A.getObjects()[0].getColor().g == 0);
+			REQUIRE(A.getObjects()[0].getColor().r == 255);
+			REQUIRE(A.getObjects()[0].getLambert() == 1);
 		}
 	}
 }
