26b1dfc3f4a5dca123fe5836f8002d2c549e9114
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-21 18:19:07 -0400

    created ray structure and dot product inline

--- ray_tracer.hpp
+++ ray_tracer.hpp
@@ -1,4 +1,11 @@
 #include "environment.hpp"
+
+struct rayType
+{
+	coords3D origin, destination;
+	rayType(coords3D o, coords3D d) : origin(o), destination(o) {}
+};
+
 typedef std::vector<std::vector<colorType>> canvasType;
 class RayTracer
 {
@@ -9,6 +16,10 @@ public:
 	void printImage(const char* path);
 
 private:
+	static inline coordsType dotp(const coords3D &a, const coords3D &b)
+	{
+		return (a.x * b.x + a.y * b.y + a.z * b.z);
+	}
 	Environment scene;
 	canvasType canvas;
 
