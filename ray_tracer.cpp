127a70346c2fc2fd22f0756986f0da63240a84a3
Isaac Becerra-Llanos <isaac211@vt.edu>
2017-04-23 18:54:17 -0400

    Implemented rudimentary drawing algorithm.

--- ray_tracer.cpp
+++ ray_tracer.cpp
@@ -13,13 +13,28 @@ void RayTracer::printImage(const QString &path)
 
 	sphereList spheres = scene.getSpheres();
 	planeList planes = scene.getPlanes();
+	lightList lights = scene.getLights();
 
 	//Iterate through each pixel; set color to sphere if intersection occurs
+	coordsType t;
+
+	//single light for test
+	Light light = lights[0];
+
 	foreach(Sphere s, spheres)
 	{
+		QColor color(
+			s.getColor().r,
+			s.getColor().g,
+			s.getColor().b);
 		for (sizeType i = 0; i < imageX; ++i)
 			for (sizeType j = 0; j < imageY; ++j)
 			{
+				const rayType ray(coords3D(i,j,0), s.getCenter());
+				if (s.intersect(ray, t))
+				{
+					pixmap.setPixel(i, j, color.rgb());
+				}
 			}
 	}
 	//Save image to a file.
